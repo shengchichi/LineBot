97a999bc7627f7c12a846e16da66a25ecc48b975
shengchichi <38219630+shengchichi@users.noreply.github.com>
2018-05-18 16:24:11 +0800

    Update LineBotSdk.py

--- LineBotSdk.py
+++ LineBotSdk.py
@@ -11,25 +11,30 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-
+paper_dict ={}
 def crawl(search_str):
     proxies = {
       'http': 'http://proxy.ncu.edu.tw/:3128'
     }
+    page = 1
     headers = {'Content-Type': 'application/json',
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36',
            'Referer': 'https://ieeexplore.ieee.org/search/searchresult.jsp?queryText=123&newsearch=true'
               } 
-    payload = {"queryText":search_str,"newsearch":"true"}
-    r = requests.post('https://ieeexplore.ieee.org/rest/search', headers=headers, json=payload, proxies = proxies)
-    #print(r.json())
-    json_data = r.text
-    dict = json.loads(json_data)
-    #print(json.loads(json_data))
-    ls = ""
-    for i in range(10):           #列出十筆資料
-        ls+="{}: {}\n\thttps://ieeexplore.ieee.org{}\n".format(i+1,dict['records'][i]['articleTitle'],dict['records'][i]['pdfLink'])
-    return ls
+    start = 0
+    while page>4:#先爬個100筆
+        payload = {"queryText":search_str,"newsearch":"true",'pageNumber':(str)page}
+        r = requests.post('https://ieeexplore.ieee.org/rest/search', headers=headers, json=payload, proxies = proxies)
+    
+        json_data = r.text
+        dict = json.loads(json_data)
+    
+        for i in range(25):           #列出十筆資料
+            #ls+="{}: {}\n".format(i+1+start,dict['records'][i]['articleTitle'])
+            paper_dict[i+1+start] = dict['records'][i]['articleTitle']
+        page+=1
+        start+=25
+    
 
 app = Flask(__name__)
 
@@ -57,16 +62,38 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text.split()[0]!='s':
+   
+    if event.message.text.split()[0] == "s":
+        start = 1
+        crawl(event.message.text.split()[1])
+        str_list = ""
+        for i in range(10):
+            str_list += paper_dict[i+start]
+            str_list += '\n'
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="輸入數字\n\n"+str_list))
+        
+    elif event.message.text == 'next':
+        if not paper_dict:
+            str_list = '"use s+" 關鍵字"'
+        else:
+            start+=10
+            for i in range(10):
+                str_list += paper_dict[i+start]
+                str_list += '\n'
+        line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="輸入數字\n\n"+str_list))
+        
+    else:
         line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='歡迎使用找論文!\n請輸入s+" 關鍵字(ex: s paper)"'))
-    if event.message.text.split()[0] == "s":
-        str_list = crawl(event.message.text.split()[1])
-        line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=str_list))
         
+    #if (int)event.message.text() >=1 and (int)event.message.text()<=10:
+     #   crawl_get((int)event.message.text())
+    
 
 
 if __name__ == "__main__":
